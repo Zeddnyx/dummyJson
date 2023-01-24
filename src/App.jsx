f989ec603f8d116a3f1960d188d63f201e8a6dd6
Zeddnyx <zeddnyx08@gmail.com>
2023-01-24 10:00:39 +0700

    done:) fix error input can just add 1 by 1, just dont make it search elemnt in component

--- src/App.jsx
+++ src/App.jsx
@@ -22,19 +22,8 @@ function App() {
   // buat state untuk tampung api dan untuk handle search nya
   // kemudian state buat handle cari nya di isi target.value 
   // dan onClick pada button di isi function apinya dengan argument dari state cari
-  const Search = () => {
-    return <div className='flex mx-auto'>
-      <input type="text"
-        className='border-2 border-black outline-none pl-2'
-        value={cari} 
-        onChange={ e => setCari(e.target.value) } 
-      />
-      <button onClick={() => api(cari)} 
-        type="submit"
-        className='bg-black text-white px-5 hover:bg-gray-700 outline-none'
-      >Search</button>
-    </div>
-  }
+  //
+  // jikq input search nya pas d ketik harus 1 1 jangan d buat conponent!!
 
   const List = () => {
     return <div className='grid gap-5 md:grid-cols-2 md:gap-36'>
@@ -51,7 +40,22 @@ function App() {
 
   return <div>
     {datas?.length > 0 
-      ? ( <div className='max-w-lg font-sans my-5 grid gap-3 mx-auto'> <Search /> <List /> </div> ) 
+      ? ( <div className='max-w-lg font-sans my-5 grid gap-3 mx-auto'>
+           <div className='flex mx-auto'>
+             <input type="text"
+               className='border-2 border-black outline-none pl-2'
+               value={cari} 
+               onChange={ e => setCari(e.target.value) } 
+             />
+             <button onClick={() => api(cari)} 
+               type="submit"
+               className='bg-black text-white px-5 hover:bg-gray-700 outline-none'
+             >Search</button>
+           </div>
+           
+           <List /> 
+
+        </div> ) 
       : ( <div className='text-center my-20'>{cari} not found</div> )
     }
   </div>
